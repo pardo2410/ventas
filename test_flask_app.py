ba0d351cba49462aeea7fbd2c7c7900bb6eae6ca
pardo2410 <pardo2410@gmail.com>
2020-05-26 01:10:04 +0200

    'Adding'

--- test_flask_app.py
+++ test_flask_app.py
@@ -1,16 +1,16 @@
+
 import unittest
 from test_base import TestFlaskBase
 
 class TestWeb(TestFlaskBase):
     def test_server_is_on(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
-    def test_rounte_index_is_hola_mundo(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, b'Hola Mundo' )
+        loquemedevuelve = self.client.get("/")
+        self.assertEqual(loquemedevuelve.status_code, 200)
 
-        
+    def test_route_index_is_lista_regiones(self):
+        loquemedevuelve = self.client.get("/")
+        self.assertEqual(loquemedevuelve.status_code, 200)
+
 
 if __name__ == '__main__':
     unittest.main()
