46cac2ef53b86e59381ebae35bb344bfa3f96ad5
pardo2410 <pardo2410@gmail.com>
2020-05-27 00:35:22 +0200

    tareas pendientes

--- run.py
+++ run.py
@@ -1,18 +1,15 @@
-  
-from flask import Flask , render_template
+from flask import Flask, render_template, request
 import csv
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    fVentas = open('./sales.csv', 'r')
+    fVentas = open('./sales10.csv', 'r')
     csvreader = csv.reader(fVentas, delimiter=',')
 
-    registros = []
     d = {}
     for linea in csvreader:
-        registros.append(linea)
         if linea[0] in d:
             d[linea[0]]['ingresos'] += float(linea[11])
             d[linea[0]]['beneficios'] += float(linea[13])
@@ -20,13 +17,22 @@ def index():
             if linea[0] != 'region':
                 d[linea[0]] = {'ingresos': float(linea[11]), 'beneficios': float(linea[13])}
     
-    return render_template('region.html', ventas = d)
-    '''
-    <abrir fichero>
-    <mientras haya registros>
-        registro = leerFichero
-        registro. separar.por commas
-        procesar ventas totales y beneficios totales
-    Montar respuesta
-    devolver respuesta.
-    '''
+
+    return render_template('region.html',ventas=d)
+
+@app.route("/paises")
+def paises():
+    region_name = request.values['region']
+
+    fVentas = open('./sales10.csv', 'r')
+    csvreader = csv.reader(fVentas, delimiter= ',')
+    d = {}
+    for linea in csvreader:
+        if linea[0] == region_name:
+            if linea[1] in d:
+                d[linea[1]]['ingresos'] += float(linea[11])
+                d[linea[1]]['beneficios'] += float(linea[13])
+            else:     
+                d[linea[1]] = {'ingresos': float(linea[11]), 'beneficios': float(linea[13])}
+
+    return d
