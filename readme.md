cf50863a9a80bff31135330bdea19a2251d79ff8
pardo2410 <pardo2410@gmail.com>
2020-06-15 23:00:23 +0200

    carda AWS

--- readme.md
+++ readme.md
@@ -1,7 +1,17 @@
 # Lista de ventas globales
 
 ## Instalación 
-Ejecutar
+1. Ejecutar
 ```
 pip install -r requirements.txt
 ```
+2. Crear _config.py
+
+Renombrar ' _config_template.py ' a ' _config.py' e informar correctamente sus claves
+
+3. Informar correctamente .evn (opcional)
+
+Renombrar ' .env_template' a ' .env' e informar las claves
+
+    -FLASK_APP = run.py
+    -FLASK_ENV = el que quieras de ' development' o ' production'
--- the_app/routes.py
+++ the_app/routes.py
@@ -1,6 +1,6 @@
 import csv, sqlite3
 from the_app import app
-from the_app.forms import ProductForm
+from the_app.forms import ProductForm, ModProductForm
 
 from flask import render_template, request, redirect, url_for
 
@@ -64,10 +64,11 @@ def addproduct():
             cur = conn.cursor()
             query = "INSERT INTO productos (tipo_producto, precio_unitario, coste_unitario) values (?, ?, ?);"
             datos = (request.values.get('tipo_producto'), request.values.get('precio_unitario'), request.values.get('coste_unitario'))
-
-            cur.execute(query, datos)
-
-            conn.commit()
+            try:
+                cur.execute(query, datos)
+                conn.commit()
+            except Exception as e:
+                print('INSERT -Error en Acceso a base de datos: {}'.format(e))
             conn.close()
 
             return redirect(url_for("productos"))
@@ -89,23 +90,31 @@ def modifica_producto():
         fila = cur.fetchone()
         conn.close()
         if fila:
-            form = ProductForm(data={'id': fila[0], 'tipo_producto': fila[1], 'precio_unitario': fila[2], 'coste_unitario': fila[3]})
-            form.submit.label.text = "Modificar"
+            form = ModProductForm(data={'id': fila[0], 'tipo_producto': fila[1], 'precio_unitario': fila[2], 'coste_unitario': fila[3]})
             return render_template('modproduct.html', form=form)
         else:
             return redirect(url_for("productos"))
     else:
-        form = ProductForm(request.form)
+           
+        form = ModProductForm(request.form)
+        
         if form.validate():
             conn = sqlite3.connect(app.config['BASE_DATOS'])
             cur = conn.cursor()
 
-            query = "UPDATE productos SET tipo_producto = ?, precio_unitario = ?, coste_unitario = ? WHERE id = ?;"
-            cur.execute(query, (form.tipo_producto.data, form.precio_unitario.data, form.coste_unitario.data, form.id.data))
-            conn.commit()
+            if request.form.get('modificar'):
+                query = "UPDATE productos SET tipo_producto = ?, precio_unitario = ?, coste_unitario = ? WHERE id = ?;"
+                tupla_data =(form.tipo_producto.data, form.precio_unitario.data, form.coste_unitario.data, form.id.data)
+            else:
+                query="DELETE FROM productos WHERE id = ?;"
+                tupla_data=(form.id.data,)
+            try:
+                cur.execute(query, tupla_data)
+                conn.commit()
+            except Exception as e:
+                print('MOD/DEL - Error en Acceso a base de datos: {}'.format(e))
             conn.close()
             return redirect(url_for("productos"))
 
         else:
-            form.submit.label.text = "Modificar"
             return render_template('modproduct.html', form=form)
